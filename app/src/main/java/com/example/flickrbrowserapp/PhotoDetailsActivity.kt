761cedc6149ad3ad14bdfd5659f76dfb4a3dd8cb
sagomezmartinez <sagomezmartinez@deloitte.com>
2023-10-10 10:12:48 -0600

    String Resources - First Commit

--- app/src/main/java/com/example/flickrbrowserapp/PhotoDetailsActivity.kt
+++ app/src/main/java/com/example/flickrbrowserapp/PhotoDetailsActivity.kt
@@ -26,9 +26,14 @@ class PhotoDetailsActivity : BaseActivity() {
 //        val photo = intent.getSerializableExtra(PHOTO_TRANSFER) as Photo
         val photo = intent.extras?.getParcelable<Photo>(PHOTO_TRANSFER) as Photo
 
-        photo_title.text = photo.title
-        photo_tags.text = photo.tags
-        photo_author.text = photo.author
+//        photo_title.text = "Title: " + photo.title
+//        photo_tags.text = "Tags: " + photo.tags
+        photo_title.text = resources.getString(R.string.photo_title_text,photo.title)
+        photo_tags.text = resources.getString(R.string.photo_tags_text,photo.tags)
+
+//        photo_author.text = "Author: " + photo.author
+
+        photo_author.text = resources.getString(R.string.photo_author_text,"my","red","car")
 
         Picasso.with(this.thumbnail.context).load(photo.link)
             .error(R.drawable.placeholder)
